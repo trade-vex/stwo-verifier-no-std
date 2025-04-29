909d0aa5a919ead92fdfa2f9bb94273839462e6e
Rahul Ghangas <rahulghangas0@gmail.com>
2025-04-30 04:32:10 +0530

    refactor: Update secure column implementation

--- src/fields/secure_column.rs
+++ src/fields/secure_column.rs
@@ -1,41 +1,52 @@
-use std::array;
-use std::iter::zip;
+use core::array;
+use core::iter::zip;
+use serde::{Deserialize, Serialize};
+use core::ops::{
+    Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
+};
+use alloc::vec::Vec;
 
-use super::m31::BaseField;
+use super::m31::M31;
 use super::qm31::SecureField;
 use super::ExtensionOf;
-// use crate::core::backend::{Col, Column, ColumnOps, CpuBackend};
+use super::backend::{Col, ColumnOps, CpuBackend};
+use num_traits::Zero;
+
+use crate::impl_field;
+use super::{FieldExpOps, ComplexConjugate};
 
 pub const SECURE_EXTENSION_DEGREE: usize =
-    <SecureField as ExtensionOf<BaseField>>::EXTENSION_DEGREE;
+    <SecureField as ExtensionOf<M31>>::EXTENSION_DEGREE;
 
 /// A column major array of `SECURE_EXTENSION_DEGREE` base field columns, that represents a column
 /// of secure field element coordinates.
-#[derive(Clone, Debug)]
-pub struct SecureColumnByCoords<B: ColumnOps<BaseField>> {
-    pub columns: [Col<B, BaseField>; SECURE_EXTENSION_DEGREE],
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize)]
+pub struct SecureColumnByCoords<B: ColumnOps<M31>> {
+    pub columns: [Col<B, M31>; SECURE_EXTENSION_DEGREE],
 }
-impl SecureColumnByCoords<CpuBackend> {
+
+impl SecureColumnByCoords<CpuBackend<M31>> {
     // TODO(first): Remove.
     pub fn to_vec(&self) -> Vec<SecureField> {
         (0..self.len()).map(|i| self.at(i)).collect()
     }
 }
-impl<B: ColumnOps<BaseField>> SecureColumnByCoords<B> {
+
+impl<B: ColumnOps<M31>> SecureColumnByCoords<B> {
     pub fn at(&self, index: usize) -> SecureField {
-        SecureField::from_m31_array(std::array::from_fn(|i| self.columns[i].at(index)))
+        SecureField::from_m31_array(core::array::from_fn(|i| self.columns[i].at(index)))
     }
 
     pub fn zeros(len: usize) -> Self {
         Self {
-            columns: std::array::from_fn(|_| Col::<B, BaseField>::zeros(len)),
+            columns: core::array::from_fn(|_| Col::<B, M31>::zeros(len)),
         }
     }
 
     /// # Safety
     pub unsafe fn uninitialized(len: usize) -> Self {
         Self {
-            columns: std::array::from_fn(|_| Col::<B, BaseField>::uninitialized(len)),
+            columns: core::array::from_fn(|_| Col::<B, M31>::uninitialized(len)),
         }
     }
 
@@ -47,7 +58,7 @@ impl<B: ColumnOps<BaseField>> SecureColumnByCoords<B> {
         self.columns[0].is_empty()
     }
 
-    pub fn to_cpu(&self) -> SecureColumnByCoords<CpuBackend> {
+    pub fn to_cpu(&self) -> SecureColumnByCoords<CpuBackend<M31>> {
         SecureColumnByCoords {
             columns: self.columns.clone().map(|c| c.to_cpu()),
         }
@@ -55,7 +66,6 @@ impl<B: ColumnOps<BaseField>> SecureColumnByCoords<B> {
 
     pub fn set(&mut self, index: usize, value: SecureField) {
         let values = value.to_m31_array();
-        #[allow(clippy::needless_range_loop)]
         for i in 0..SECURE_EXTENSION_DEGREE {
             self.columns[i].set(index, values[i]);
         }
@@ -63,9 +73,10 @@ impl<B: ColumnOps<BaseField>> SecureColumnByCoords<B> {
 }
 
 pub struct SecureColumnByCoordsIter<'a> {
-    column: &'a SecureColumnByCoords<CpuBackend>,
+    column: &'a SecureColumnByCoords<CpuBackend<M31>>,
     index: usize,
 }
+
 impl Iterator for SecureColumnByCoordsIter<'_> {
     type Item = SecureField;
 
@@ -79,7 +90,8 @@ impl Iterator for SecureColumnByCoordsIter<'_> {
         }
     }
 }
-impl<'a> IntoIterator for &'a SecureColumnByCoords<CpuBackend> {
+
+impl<'a> IntoIterator for &'a SecureColumnByCoords<CpuBackend<M31>> {
     type Item = SecureField;
     type IntoIter = SecureColumnByCoordsIter<'a>;
 
@@ -90,22 +102,32 @@ impl<'a> IntoIterator for &'a SecureColumnByCoords<CpuBackend> {
         }
     }
 }
-impl FromIterator<SecureField> for SecureColumnByCoords<CpuBackend> {
-    fn from_iter<I: IntoIterator<Item = SecureField>>(iter: I) -> Self {
+
+impl FromIterator<M31> for SecureColumnByCoords<CpuBackend<M31>> {
+    fn from_iter<I: IntoIterator<Item = M31>>(iter: I) -> Self {
         let values = iter.into_iter();
         let (lower_bound, _) = values.size_hint();
-        let mut columns = array::from_fn(|_| Vec::with_capacity(lower_bound));
+        let mut vecs: [Vec<M31>; SECURE_EXTENSION_DEGREE] = array::from_fn(|_| Vec::with_capacity(lower_bound));
 
         for value in values {
-            let coords = value.to_m31_array();
-            zip(&mut columns, coords).for_each(|(col, coord)| col.push(coord));
+            let coords = [value, M31::zero(), M31::zero(), M31::zero()];
+            zip(&mut vecs, coords).for_each(|(col, coord)| col.push(coord));
         }
+
+        let columns = array::from_fn(|i| unsafe {
+            let mut col = Col::<CpuBackend<M31>, M31>::uninitialized(vecs[i].len());
+            for (j, val) in vecs[i].iter().enumerate() {
+                col.set(j, *val);
+            }
+            col
+        });
 
         SecureColumnByCoords { columns }
     }
 }
-impl From<SecureColumnByCoords<CpuBackend>> for Vec<SecureField> {
-    fn from(column: SecureColumnByCoords<CpuBackend>) -> Self {
+
+impl From<SecureColumnByCoords<CpuBackend<M31>>> for Vec<SecureField> {
+    fn from(column: SecureColumnByCoords<CpuBackend<M31>>) -> Self {
         column.into_iter().collect()
     }
 }
